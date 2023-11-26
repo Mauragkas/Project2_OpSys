2e52551323ca18467d56860c5f8da7f33c401f09
Mauragkas <stathpan03@gmail.com>
2023-11-27 00:16:13 +0200

    first commit

---
+++ scheduler/run.sh
@@ -0,0 +1,9 @@
+# ./scheduler BATCH homogeneous.txt
+# ./scheduler RR 1000 homogeneous.txt
+
+# ./scheduler BATCH reverse.txt
+# ./scheduler RR 1000 reverse.txt
+
+# ./scheduler_io BATCH mixed.txt
+# ./scheduler_io RR 1000 mixed.txt
+
